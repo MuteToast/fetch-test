975280bcfe03fe04e0e7ab7a90089f9be41e3df4
MuteToast <theoristo7@gmail.com>
2026-01-14 14:10:35 +0100

    test 3

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 // mockapi.ntig.dev/students/2000
 // mockapi.ntig.dev/courses/3000
 
+// only console log fetch
+
 fetch('https://mockapi.ntig.dev/students/2000')
     .then(response => response.json())
-    .then(studentData => {
-        console.log('Student Data:', studentData);
-        return fetch(`https://mockapi.ntig.dev/courses/${studentData.courseId}`);
-    })
-    .then(response => response.json())
-    .then(courseData => {
-        console.log('Course Data:', courseData);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+    .then(data => console.log(data))
+    .catch(error => console.error('Error fetching student data:', error));
