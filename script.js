aad23610a20f5cfc161461f7c6f75492cbbe2e27
MuteToast <theoristo7@gmail.com>
2026-01-14 14:12:32 +0100

    test 4

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ fetch('https://mockapi.ntig.dev/students/2000')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error('Error fetching student data:', error));
+
+    fetch('https://mockapi.ntig.dev/courses/3000')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error fetching student data:', error));
